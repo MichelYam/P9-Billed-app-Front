d96122a834c398b04d91e56425661e11d00d43bd
MichelYAM <michel.yam92@gmail.com>
2022-09-21 00:28:34 +0200

    add new test

--- src/__tests__/NewBill.test.js
+++ src/__tests__/NewBill.test.js
@@ -2,71 +2,98 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
 import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
+import store from "../__mocks__/store.js"
+import router from "../app/Router.js";
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     test("Then, the form should Appear", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+      // const html = NewBillUI()
+      // document.body.innerHTML = html
+      // //to-do write assertion
 
+      // const form = screen.getByTestId("form-new-bill");
+      // const handleSubmit = jest.fn((e) => e.preventDefault());
+
+      // form.addEventListener("submit", handleSubmit);
+      // fireEvent.submit(form);
+      // expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    })
+    test("Then mail icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const mailIcon = screen.getByTestId('icon-mail')
+      console.log(mailIcon)
+      const iconActive = mailIcon.classList.contains("active-icon");
+      expect(iconActive).toBeTruthy();
     })
   })
-  //    describe("When I choose a file with an incorret extension", () => {
-  //      test("Then it should display error message", () => {
-  //        const html = NewBillUI()
-  //        document.body.innerHTML = html
-  //        const onNavigate = (pathname) => {
-  //          document.body.innerHTML = ROUTES({ pathname });
-  //        };
-  //        //Create element to dipslay error message
-  //        const newContent = document.createTextNode('Error file (this extension is not allowed)');
-  //        const p = document.createElement('p')
-  //        p.appendChild(newContent)
-  //        p.setAttribute("id", "error-msg")
+  describe("When I am on NewBill Page and I choose a file with an incorret extension", () => {
+    test("Then it should display error message", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
 
-  //        const inputFile = screen.getByTestId("file")
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
 
-  //        const newBill = new NewBill({
-  //          document,
-  //          onNavigate,
-  //          store,
-  //          localStorage: window.localStorage,
-  //        });
-  //        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
 
-  //        inputFile.addEventListener("change", handleChangeFile)
-  //        document.body.insertAfter(newDiv, currentDiv);
-  //        expect(handleChangeFile).toHaveBeenCalled();
-  //        expect(input.files[0].name).toBe("file.pdf");
-  //        expect(inputFile.innerHTML += p).toHaveBeenCalled();
-  //      })
-  //    })
-  //    // test d'intégration POST
-  //    describe("When I do fill fields in correct format and I click on button Send", () => {
-  //      test("Then it should add the new bills to the list", () => {
-  //        const html = NewBillUI()
-  //        document.body.innerHTML = html
-  //        const onNavigate = (pathname) => {
-  //          document.body.innerHTML = ROUTES({ pathname });
-  //        };
-  //        const form = screen.getByTestId("form-new-bill");
-  //        const newBill = new NewBill({
-  //          document,
-  //          onNavigate,
-  //          store,
-  //          localStorage: window.localStorage,
-  //        });
-  //        //recupere les valeurs entrées
-  //        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
-  //        const submitForm = screen.getByTestId("form-new-bill");
-  //        form.addEventListener("submit", handleSubmit);
-  //        fireEvent.submit(submitForm);
-  //        expect(handleSubmit).toHaveBeenCalled();
-  //      })
-  //    })
+      const inputFile = screen.getByTestId("file");
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+
+      inputFile.addEventListener("change", handleChangeFile)
+
+      fireEvent.change(inputFile, { target: { files: [new File(['input'], 'image.png', { type: 'image/png' })], } });
+
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(inputFile.files[0].name).toBe("image.png");
+    })
+  })
+     // test d'intégration POST
+     describe("When I do fill fields in correct format and I click on button Send", () => {
+       test("Then it should add the new bill to the list", () => {
+         const html = NewBillUI()
+         document.body.innerHTML = html
+
+         const onNavigate = (pathname) => {
+           document.body.innerHTML = ROUTES({ pathname });
+         };
+
+         const form = screen.getByTestId("form-new-bill");
+         const newBill = new NewBill({
+           document,
+           onNavigate,
+           store,
+           localStorage: window.localStorage,
+         });
+
+         //recupere les valeurs entrées
+         const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+         const submitForm = screen.getByTestId("form-new-bill");
+         form.addEventListener("submit", handleSubmit);
+         fireEvent.submit(submitForm);
+         expect(handleSubmit).toHaveBeenCalled();
+       })
+     })
 })
